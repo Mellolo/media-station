dfbcc96fd4f93b3af0aabf601da7fd9e8db301b4
mellolo <21921066@zju.edu.cn>
2025-08-13 00:22:54 +0800

    facade层抽象

--- enum/permissionLevel.go
+++ enum/permissionLevel.go
@@ -9,7 +9,7 @@ var PermissionLevels = []string{
 }
 
 const (
-	PermissionPublic    = "public "   // 公开：所有用户可访问
+	PermissionPublic    = "public"    // 公开：所有用户可访问
 	PermissionLogin     = "login"     // 登录用户：仅登录用户可访问
 	PermissionVIP       = "vip"       // VIP 用户：需为 VIP 身份
 	PermissionPrivate   = "private"   // 私密：仅上传者或指定用户可访问
