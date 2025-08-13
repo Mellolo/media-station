5f816aac05f0af8bcf4bd8b37bbdbafda1ce3d6e
mellolo <21921066@zju.edu.cn>
2025-08-13 20:20:14 +0800

    作品增删

--- routers/commentsRouter.go
+++ routers/commentsRouter.go
@@ -135,6 +135,15 @@ func init() {
 
 	beego.GlobalControllerRouter["media-station/controllers:VideoAuthController"] = append(beego.GlobalControllerRouter["media-station/controllers:VideoAuthController"],
 		beego.ControllerComments{
+			Method:           "Update",
+			Router:           `update`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["media-station/controllers:VideoAuthController"] = append(beego.GlobalControllerRouter["media-station/controllers:VideoAuthController"],
+		beego.ControllerComments{
 			Method:           "Upload",
 			Router:           `upload`,
 			AllowHTTPMethods: []string{"post"},
